2604dba4cbd05d8f3712582330739e5d3928cc0b
KentHirose <k.hirose77777@gmail.com>
2024-05-27 00:13:21 +0900

    パッケージ名を修正しました

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="openrouter_selector",
+    name="openrouter-selector",
     author="kent",
     description="LLM connection using OpenRouter with a single library.",
     version="0.1.0",
-    packages=["openrouter_selector"],
+    packages=["openrouter-selector"],
     install_requires=["langchain"],
 )
 
